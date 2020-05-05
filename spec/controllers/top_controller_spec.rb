909759ba84bbad92476747101395605523832f9a
admin <admin@nightonly.com>
2020-05-05 12:06:33 +0900

    origin#31,32 トップページ実装、テストコード作成

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
+  render_views
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index/トップページにアクセス' do
+    it 'returns http success/HTTPステータスコードが200' do
       get :index
       expect(response).to have_http_status(:success)
     end
+    it 'ページ中にHello World!が含まれる' do
+      get :index
+      expect(response.body).to match(/Hello World!/)
+    end
   end
-
 end
--- spec/views/top/index.html.erb_spec.rb
+++ spec/views/top/index.html.erb_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "top/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'top/index.html.erb', type: :view do
 end
