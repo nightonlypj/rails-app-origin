4c95420da7d18c7134fa580b7fe067869c9c5510
admin <admin@nightonly.com>
2020-05-11 10:29:22 +0900

    origin#128 管理者のアカウント登録禁止、routingテスト追加

---
+++ spec/routing/admin_users/sessions_routing_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe AdminUsers::SessionsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #new' do
+      expect(get: '/admin_users/sign_in').to route_to('admin_users/sessions#new')
+    end
+    it 'routes to #create' do
+      expect(post: '/admin_users/sign_in').to route_to('admin_users/sessions#create')
+    end
+    it 'routes to #destroy' do
+      expect(delete: '/admin_users/sign_out').to route_to('admin_users/sessions#destroy')
+    end
+  end
+end
