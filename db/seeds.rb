767bc132460ca3bc71caa9d44214f71a6f4a0389
admin <admin@nightonly.com>
2020-05-14 08:48:20 +0900

    origin#128 管理者用の初期アカウント作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dir.glob("#{Rails.root}/db/seed/*.yml").each do |filename|
+  puts 'filename: ' + filename
+
+  target_model = File.basename(filename, '.yml').classify.constantize
+  puts 'model: ' + target_model.to_s
+
+  File.open(filename) do |file_contents|
+    yaml_contents = YAML.safe_load(file_contents)
+    yaml_contents.each do |yaml_record|
+      id = yaml_record['id']
+
+      if target_model.find_by(id: id)
+        puts 'id: ' + id.to_s + ' ... Skip create'
+        next
+      end
+
+      puts 'id: ' + id.to_s + ' ... Create'
+      target_model.create(yaml_record)
+    end
+  end
+end
