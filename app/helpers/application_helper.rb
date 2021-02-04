5f7f5c14a59eeca19a8bd22933772604a71e876a
admin <admin@nightonly.com>
2021-02-04 20:27:29 +0900

    origin#249 デザイン追加修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,28 +12,22 @@ module ApplicationHelper
 
   # バリデーション表示のクラス名を返却
   def validate_class_name(enabled, resource, key, subkey = nil)
-    if enabled && resource.errors.any?
-      if subkey.present?
-        resource.errors[key].any? || resource.errors[subkey].any? ? ' is-invalid' : ' is-valid'
-      else
-        resource.errors[key].any? ? ' is-invalid' : ' is-valid'
-      end
+    return '' unless enabled
+
+    if subkey.present?
+      resource.errors[key].any? || resource.errors[subkey].any? ? ' is-invalid' : ' is-valid'
     else
-      ''
+      resource.errors[key].any? ? ' is-invalid' : ' is-valid'
     end
   end
 
   # パスワードのバリデーション表示のクラス名を返却 # Tips: パスワードは再入力で復元しない為
-  def validate_password_class_name(enabled, resource)
-    enabled && resource.errors.any? ? ' is-invalid' : ''
+  def validate_password_class_name(enabled)
+    enabled ? ' is-invalid' : ''
   end
 
   # 入力項目のサイズクラス名を返却
   def input_size_class_name(resource, key)
-    if resource.errors.any?
-      resource.errors[key].any? ? ' mb-5' : ' mb-3'
-    else
-      ' mb-3'
-    end
+    resource.errors.any? && resource.errors[key].any? ? ' mb-5' : ' mb-3'
   end
 end
